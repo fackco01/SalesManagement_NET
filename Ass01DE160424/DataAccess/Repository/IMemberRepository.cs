bb346b2ac96a61e9d637f640dbfd6920cd646b4a
fackco01 <duanhotrong@gmail.com>
2023-08-03 17:17:52 +0700

    Commit project

---
+++ Ass01DE160424/DataAccess/Repository/IMemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using BussinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccess.Repository
+{
+    internal interface IMemberRepository
+    {
+        Task<MemberObject> GetMemberById(int memberId);
+        Task<IEnumerable<MemberObject>> GetMembers();
+        Task<MemberObject> SaveMember(MemberObject member);
+        Task DeleteMember(int memberId);
+    }
+}
---
+++ Ass01DE160424/DataAccess/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using BussinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<ProductObject> GetProducts();
+        ProductObject GetProductById(int productId);
+        void SaveProduct(ProductObject product);
+    }
+}
